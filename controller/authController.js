0daa1653307527d1b9132ee5b81a2416ee492576
kmiqbal19 <km.iqbal01@gmail.com>
2022-05-18 00:37:23 +0200

    implemented jwt

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,15 @@
 const User = require("../model/userModel");
 const AppError = require("../util/appError");
-
+const jwt = require("jsonwebtoken");
 exports.signup = async (req, res, next) => {
   try {
     const newUser = await User.create(req.body);
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
     res.status(200).json({
       status: "success",
+      token,
       data: {
         user: newUser,
       },
@@ -24,17 +28,17 @@ exports.login = async (req, res, next) => {
     }
     // Check if the user exist and password matches
     const user = await User.findOne({ email });
-    const check = await user.checkPassword(password, user.password);
+    const check = user && (await user.checkPassword(password, user.password));
 
     if (!user || check === false) {
       return next(new AppError("Incorrect email or Password", 401));
     }
     res.status(200).json({
       status: "success",
+      message: "You are successfully logged in to your account.",
       data: {
         user,
       },
-      message: "You are successfully logged In..",
     });
   } catch (err) {
     next(err);
