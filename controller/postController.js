596d1dc709e287c84c8a15a8f8caa1211e375a28
kmiqbal19 <km.iqbal01@gmail.com>
2022-05-18 03:54:23 +0200

    completed crud application for post

--- controller/postController.js
+++ controller/postController.js
@@ -1,5 +1,7 @@
 const Post = require("../model/postModel");
+const AppError = require("../util/appError");
 
+// GET ALL POSTS
 exports.getPosts = async (req, res, next) => {
   try {
     const posts = await Post.find();
@@ -13,7 +15,7 @@ exports.getPosts = async (req, res, next) => {
     next(err);
   }
 };
-
+// CREATE NEW POST
 exports.createPost = async (req, res, next) => {
   try {
     const newPost = new Post(req.body);
@@ -24,6 +26,61 @@ exports.createPost = async (req, res, next) => {
         post: savedPost,
       },
     });
+  } catch (err) {
+    next(err);
+  }
+};
+// UPDATE EXISTING POST
+exports.updatePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    console.log(post);
+    if (!post) return next(new AppError("No post found!", 404));
+    if (post.username === req.body.username) {
+      try {
+        const newPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+        res.status(200).json({
+          status: "success",
+          data: {
+            post: newPost,
+          },
+        });
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      res.status(401).json({
+        status: "unauthorized",
+        message: "You can only update your post!",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+// DELETE POST
+exports.deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (req.body.username === post.username) {
+      try {
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+          status: "success",
+          message: "Your post has been deleted successfully!",
+        });
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      res.status(401).json({
+        status: "unauthorized",
+        message: "You can only delete your own post!",
+      });
+    }
   } catch (err) {
     next(err);
   }
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,9 +1,11 @@
-const postsController = require("../controller/postController");
 const express = require("express");
+const postsController = require("../controller/postController");
 
 const router = express.Router();
 
 router.get("/", postsController.getPosts);
 router.post("/", postsController.createPost);
+router.put("/:id", postsController.updatePost);
+router.delete("/:id", postsController.deletePost);
 
 module.exports = router;
